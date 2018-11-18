1de232d3f6788b99b47ffb5936688ee2f03dcf10
Jaydip Patel <japa8522@gmail.com>
2018-11-17 21:28:58 -0700

    reg line added in the plot(x,y)

--- rnorm test.R
+++ rnorm test.R
@@ -5,3 +5,11 @@ plot(x)
 
 y = rnorm(100, mean =10, sd = 3)
 plot(x,y)
+
+df <- data.frame(x,y)
+
+lm1 = lm(y~x, data = df)
+plot(x,y)
+abline(coefficients(lm1)[1], coefficients(lm1)[2], col= 'purple')
+
+#abline is reg line.
